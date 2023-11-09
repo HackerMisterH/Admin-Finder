92dc5de6d6984bea02fa0a323c954304bf92e80c
HackerMisterH <143317889+HackerMisterH@users.noreply.github.com>
2023-11-09 13:09:55 +0600

    Add files via upload

---
+++ Admin-Finder_install.py
@@ -0,0 +1,8 @@
+from os import system
+system("clear")
+system("pip install requests")
+system("pip install pyfiglet")
+system("pip install termcolor")
+system("mv Document Admin-Finder.py")
+print("Now run python Admin-Finder.py")
+system("rm -rf Admin-Finder_install.py")
